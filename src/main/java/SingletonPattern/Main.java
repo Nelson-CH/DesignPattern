78df606843cf43a7ce6ad46a59f24252537f9f34
Nel <chbhg@yahoo.com.tw>
2025-09-29 16:07:37 +0800

    16.Singleton Pattern

---
+++ src/main/java/SingletonPattern/Main.java
@@ -0,0 +1,24 @@
+package SingletonPattern;
+
+class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1 == s2);
+    }
+}
